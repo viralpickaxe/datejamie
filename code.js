608ac18d0ea225d4feba811d9f8d908103ca39a1
Jamie Davies <jamie@iamjamiedavies.com>
2018-02-16 02:20:57 +0000

    Form working

---
+++ code.js
@@ -0,0 +1,35 @@
+var $form = $('form');
+
+$('#submit-form').on('click', function(e) {
+  e.preventDefault();
+  var values = $form.serializeObject();
+
+  for (var i in values) {
+    if (values[i] === '') {
+      $(`[name="${i}"]`).css('border-color', 'red');
+      return false;
+    }
+  }
+
+  var jqxhr = $.ajax({
+    url: 'https://hooks.zapier.com/hooks/catch/2970859/zfhnod/',
+    method: "POST",
+    dataType: "json",
+    data: values
+  }).done(function(data) {
+    $('form').html('<div class="success">Thanks, speak soon.</div>');
+  })
+});
+
+$('input, textarea').keyup(function(e) {
+  $(e.target).css('border-color', '#f5f5f5');
+})
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+  event.preventDefault();
+  $('.slide.form').css('display', 'flex')
+
+  $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+  }, 300);
+});
